db3732ffe8b151fc89aefd4968db9d73c69758df
Benoît Béranger <benoit.beranger@icm-institute.org>
2019-03-12 14:56:10 +0100

    GUI for EstimateT1 is ready

--- mp2rage_defaults.m
+++ mp2rage_defaults.m
@@ -12,5 +12,11 @@ mp.rmbg.regularization  = 1;
 mp.rmbg.output.prefix   = 'clean_';
 mp.rmbg.output.filename = 'clean_UNI';
 
+% Estimate T1
+%==========================================================================
+mp.estimateT1.outputT1.prefix   = 'qT1_';
+mp.estimateT1.outputT1.filename = 'qT1';
+mp.estimateT1.outputR1.prefix   = 'qR1_';
+mp.estimateT1.outputR1.filename = 'qR1';
 
 end % function
